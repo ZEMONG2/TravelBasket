9ebafcde371539569962ef90fc8272630ed1b764
ZEMONG2 <zerosole0@gmail.com>
2022-08-10 08:58:32 +0900

    게시판 입력 조회

--- server/index.js
+++ server/index.js
@@ -1,19 +1,43 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
-const PORT = process.env.port || 4000;
+const bodyParser = require("body-parser");
+const PORT = process.env.port || 8000;
+const cors = require("cors");
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "123456",
-  database: "bbs",
+  host: "210.114.22.116",
+  user: "js_team_5",
+  password: "tb123456",
+  database: "js_team_5",
 });
 
-app.get("/", (req, res) => {
-  const sqlQuery = "INSERT INTO requested (rowno) VALUES (1)";
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/api/select", (req, res) => {
+  const sqlQuery = "SELECT * FROM test;";
   db.query(sqlQuery, (err, result) => {
-    res.send("success!");
+    res.send(result);
+  });
+});
+
+// app.post("/api/delete", (req, res) => {
+//   const boardNum = req.body.num;
+//   const sqlQuery = "DELETE FROM test WHERE num = ?";
+//   db.query(sqlQuery, boardNum, (err, result) => {
+//     res.send("delete success!");
+//   });
+// });
+
+app.post("/api/insert", (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const id = req.body.id;
+  const sqlQuery = "INSERT INTO test (title, content, id) VALUES (?,?,?)";
+  db.query(sqlQuery, [title, content, id], (err, result) => {
+    res.send("insert success!");
   });
 });
 
--- travel-basket/src/App.js
+++ travel-basket/src/App.js
@@ -1,14 +1,117 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useEffect, useState, useRef } from "react";
 import Axios from "axios";
 
 function App() {
-  const submitTest = () => {
-    Axios.get("http://localhost:4000/", {}).then(() => {
+  const [boardData, setBoardData] = useState({
+    title: "",
+    content: "",
+    id: "",
+  });
+  // const delRef = useRef([]);
+  const [boardDataView, setBoardDataView] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:8000/api/select").then((res) => {
+      setBoardDataView(res.data);
+    });
+  }, [boardDataView]);
+
+  const boardSubmit = () => {
+    Axios.post("http://localhost:8000/api/insert", {
+      title: boardData.title,
+      content: boardData.content,
+      id: boardData.id,
+    }).then(() => {
       alert("등록 완료!");
     });
+    const nextBoardData = {
+      ...boardData,
+      title: "",
+      content: "",
+      id: "",
+    };
+    setBoardData(nextBoardData);
   };
-  return <button onClick={submitTest}>글쓰기</button>;
+
+  const getBoardData = (e) => {
+    const { name, value } = e.target;
+    setBoardData({
+      ...boardData,
+      [name]: value,
+    });
+  };
+
+  // function boardDelete(idx) {
+  //   Axios.post("http://localhost:8000/api/delete/", {
+  //     num: idx,
+  //   }).then(() => {
+  //     alert("삭제 완료!");
+  //   });
+  // }
+  return (
+    <div>
+      <h1>BOARD LIST</h1>
+      <div className="board-list">
+        <table border="1px solid">
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>내용</th>
+            <th>작성자</th>
+            <th></th>
+          </tr>
+
+          {boardDataView.map((boardDataView) => (
+            <tr key={boardDataView.num}>
+              <td>{boardDataView.num}</td>
+              <td>{boardDataView.title}</td>
+              <td>{boardDataView.content}</td>
+              <td>{boardDataView.id}</td>
+              <td>
+                {/* <button
+                  key={boardDataView.num}
+                  ref={(ref) => (delRef.current[boardDataView.num] = ref)}
+                  className="delete-button"
+                  onClick={boardDelete(boardDataView.num)}
+                >
+                  삭제
+                </button> */}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+      <div className="board-form">
+        <input
+          className="title-input"
+          type="text"
+          placeholder="제목"
+          onChange={getBoardData}
+          value={boardData.title}
+          name="title"
+        />
+        <input
+          className="content-input"
+          type="text"
+          placeholder="내용"
+          onChange={getBoardData}
+          value={boardData.content}
+          name="content"
+        />
+        <input
+          className="id-input"
+          type="text"
+          placeholder="아이디"
+          onChange={getBoardData}
+          value={boardData.id}
+          name="id"
+        />
+      </div>
+      <button className="submit-button" onClick={boardSubmit}>
+        입력
+      </button>
+    </div>
+  );
 }
 
 export default App;
