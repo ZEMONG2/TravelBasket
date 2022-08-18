90d02bf729eeae3b1c2c9df833d44582519c0a1d
SoonAh-Noh <109115744+SoonAh-Noh@users.noreply.github.com>
2022-08-18 10:56:23 +0900

    노순아_json,Appjs,indexcss

--- travel-basket/src/App.js
+++ travel-basket/src/App.js
@@ -1,21 +1,42 @@
 import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Basket from "./components/Basket";
 import Main from "./components/Main";
 import Schedule from "./components/Schedule";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Review from "./components/Review";
 import NotFound from "./components/NotFound";
+import "./css/App.scss";
+import background from "../src/img/summer.mp4";
+import Loading from "./components/Loading";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/review" element={<Review />} />
-      <Route path="/schedule" element={<Schedule />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+    <>
+      <video autoPlay loop muted playsInline>
+        <source src={background} type="video/mp4"></source>
+      </video>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          {/* 회원가입 */}
+          <Route path="/register" element={<Register />} />
+          {/* 후기 게시판 */}
+          <Route path="/review" element={<Review />} />
+          {/* 일정 공유 게시판 */}
+          <Route path="/schedule" element={<Schedule />} />
+          {/* 장바구니 */}
+          <Route path="/basket" element={<Basket />} />
+          {/* 404 */}
+          <Route path="/*" element={<NotFound />} />
+          {/* 로딩 */}
+          <Route path="/loading" element={<Loading />} />
+        </Routes>
+      </div>
+    </>
   );
 };
 
