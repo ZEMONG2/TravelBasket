1878426a412c2b124fe25c991efbcb84b493ef25
projectjh <jhhhh9201@gmail.com>
2022-09-02 20:16:09 +0900

    last

--- travel-basket/src/components/Board/ReviewStorage.js
+++ travel-basket/src/components/Board/ReviewStorage.js
@@ -57,7 +57,9 @@ const ReviewStorage = () => {
     setPage(page);
   };
 
-  return (
+
+  if (myReview.length == 0) {
+    return (
     <div className="ReviewStorage">
       <h1>후기 보관함</h1>
 
@@ -89,52 +91,92 @@ const ReviewStorage = () => {
             </tr>
           </thead>
           <tbody>
-            {myReview
-              .slice(
-                reviewSlice * (page - 1),
-                reviewSlice * (page - 1) + reviewSlice,
-              )
-              .map((myReview) => {
-                return (
-                  <tr>
-                    <td>{MRIdx--}</td>
-                    <td
-                      className="storageTitle"
-                      onClick={() =>
-                        navigate(`/review/view/${myReview.REVIEW_IDX}`)
-                      }
-                    >
-                      {myReview.REVIEW_TITLE}
-                    </td>
-                    <td>
-                      {
-                        List.reviewTime(myReview.REVIEW_DATE)
-                          .toString()
-                          .split('T')[0]
-                      }
-                    </td>
-                    <td>{myReview.REVIEW_CNT}</td>
-                    <td>{myReview.REVIEW_LIKE}</td>
-                    <td>{myReview.COMMENT_CNT}</td>
-                  </tr>
-                );
-              })}
+             <td colSpan="6"><p className="NoList">작성한 게시물이 없습니다.</p></td>
           </tbody>
         </table>
       </div>
-
-      <Pagination
-        activePage={page}
-        itemCountPerPage={reviewSlice}
-        totalItemsCount={myReview.length}
-        firstPageText={'<<'}
-        prevPageText={'<'}
-        nextPageText={'>'}
-        lastPageText={'>>'}
-        onChange={handlePage}
-      />
     </div>
-  );
+    );
+  } else {
+    return (
+      <div className="ReviewStorage">
+        <h1>후기 보관함</h1>
+
+        <p className="Review_count">총 {countMR} 개</p>
+
+        <div className="ReviewList">
+          <table
+            className="storageTable"
+            border="0"
+            cellPadding="0"
+            cellSpacing="0"
+          >
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>제목</th>
+                <th onClick={checkSort} id="REVIEW_DATE">
+                  작성날짜
+                </th>
+                <th onClick={checkSort} id="REVIEW_CNT">
+                  조회수
+                </th>
+                <th onClick={checkSort} id="REVIEW_LIKE">
+                  좋아요수
+                </th>
+                <th onClick={checkSort} id="COMMENT_CNT">
+                  댓글수
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {myReview
+                .slice(
+                  reviewSlice * (page - 1),
+                  reviewSlice * (page - 1) + reviewSlice,
+                )
+                .map((myReview) => {
+                  return (
+                    <tr>
+                      <td>{MRIdx--}</td>
+                      <td
+                        className="storageTitle"
+                        onClick={() =>
+                          navigate(`/review/view/${myReview.REVIEW_IDX}`)
+                        }
+                      >
+                        {myReview.REVIEW_TITLE}
+                      </td>
+                      <td>
+                        {
+                          List.reviewTime(myReview.REVIEW_DATE)
+                            .toString()
+                            .split('T')[0]
+                        }
+                      </td>
+                      <td>{myReview.REVIEW_CNT}</td>
+                      <td>{myReview.REVIEW_LIKE}</td>
+                      <td>{myReview.COMMENT_CNT}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+
+        <Pagination
+          activePage={page}
+          itemCountPerPage={reviewSlice}
+          totalItemsCount={myReview.length}
+          firstPageText={'<<'}
+          prevPageText={'<'}
+          nextPageText={'>'}
+          lastPageText={'>>'}
+          onChange={handlePage}
+        />
+      </div>
+    );
+  }
 };
 
 export default ReviewStorage;
